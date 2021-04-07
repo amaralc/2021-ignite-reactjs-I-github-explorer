a5e1bfb2aa1955925952cee901bfa18f923f6d0b
amaralc <amaral.calil@gmail.com>
2021-04-07 16:29:13 +0000

    feature: create component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './styles/globals.scss'
+import {RepositoryList} from './components/RepositoryList'
 
 export function App() {
-    return <h1>Hello World!</h1>
+    return <RepositoryList/>
 }
